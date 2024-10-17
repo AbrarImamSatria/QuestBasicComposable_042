6fcda9b9fbff62d55b852df3c99b258bae06ed71
abrarimamsatria09 <092004abrar@gmail.com>
2024-10-17 21:32:02 +0700

    menambahkan file gambar dan mengatur besarnya

--- app/src/main/java/com/example/pertemuan3/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan3/MainActivity.kt
@@ -4,22 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pertemuan3.ui.theme.Pertemuan3Theme
 
@@ -61,6 +66,11 @@ fun BasicCompose(abrar:Modifier = Modifier){
              color = Color.Blue,
              fontFamily = FontFamily.Monospace
          )
+     )
+     Image(
+         painter = painterResource(id = R.drawable.android),
+         contentDescription = null,
+         modifier = Modifier.size(250.dp)
      )
  }
 }
