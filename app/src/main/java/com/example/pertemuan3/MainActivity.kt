a740e54311149907220185c8bd6c339fd3896535
abrarimamsatria09 <092004abrar@gmail.com>
2024-10-17 21:07:38 +0700

    membuat column dan mengatur posisi elemen vertical di center

--- app/src/main/java/com/example/pertemuan3/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan3/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.pertemuan3.ui.theme.Pertemuan3Theme
@@ -32,7 +35,10 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun BasicCompose(abrar:Modifier = Modifier){
-
+ Column (
+     verticalArrangement = Arrangement.Center,
+ ){
+ }
 }
 
 
