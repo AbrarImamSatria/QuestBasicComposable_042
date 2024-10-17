6e2cc891b0fc43b75e46fc69e949ba567bdf3c2a
abrarimamsatria09 <092004abrar@gmail.com>
2024-10-17 21:18:58 +0700

    menambahkan teks pemrograman dan mengatur besar huruf

--- app/src/main/java/com/example/pertemuan3/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan3/MainActivity.kt
@@ -13,7 +13,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.pertemuan3.ui.theme.Pertemuan3Theme
 
 class MainActivity : ComponentActivity() {
@@ -40,6 +46,9 @@ fun BasicCompose(abrar:Modifier = Modifier){
      modifier = Modifier.fillMaxSize(),
      horizontalAlignment = Alignment.CenterHorizontally,
  ){
+     Text(text = "Pemrograman", modifier = abrar,
+         fontSize = 50.sp,
+     )
  }
 }
 
