aa183a50120a959cb735e6d9321397e68e5fcfb8
abrarimamsatria09 <092004abrar@gmail.com>
2024-10-17 21:37:52 +0700

    menambahkan gambar dan mengatur stylenya

--- app/src/main/java/com/example/pertemuan3/MainActivity.kt
+++ app/src/main/java/com/example/pertemuan3/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.colorResource
@@ -87,6 +89,13 @@ fun BasicCompose(abrar:Modifier = Modifier){
          style = TextStyle(
              fontSize = 40.sp,
          )
+     )
+     Image(
+         painter = painterResource(id = R.drawable.gambar),
+         contentDescription = null,
+         modifier = Modifier
+             .size(350.dp)
+             .clip(CircleShape)
      )
  }
 }
